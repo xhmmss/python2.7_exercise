b8d2ed1cb9ea3b2b0263bd497be0ef889ae090f9
piggy <xhmmss@qq.com>
2019-05-20 10:02:20 +0800

    2

---
+++ liaoxuefeng/data_type.py
@@ -0,0 +1,20 @@
+string1 = "I\'m ok!"
+print string1
+
+string2 = r"\\//\/\/"
+print string2
+
+string3 = '''
+life is boring
+life is interesting
+'''
+print string3
+
+PI = 3.1415926 #use upper to denote constant data.
+
+number1 = 10 / 3
+number2 = 10.0 / 3
+number3 = 10. / 3
+number4 = 10 / 3.
+number5 = 10 / 3.0
+print number1,number2,number3,number4,number5
